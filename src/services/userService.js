0f4d49309084e4a8264d3f52f066293cb76952a2
Ivan K. Montes de oca <ivaanmontess2@hotmail.com>
2025-05-31 20:23:31 -0300

    arreglos de errores

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://backend-gimnasio-fqfn.onrender.com';
+const API_URL = 'https://backend-gimnasio-fqfn.onrender.com/api/usuarios';
 
 export const obtenerUsuarios = async () => {
   try {
